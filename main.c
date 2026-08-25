4f8220d50bf9d1549356c0389865455687606b7f
Kev <kevin.hulbert93@gmail.com>
2026-08-25 13:56:57 +1000

    add shell.h and main.c to Repo

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "shell.h"
+/*
+ *
+ *
+ */
+
+int main(int ac, char **argv)
+{
+    char *prompt = "($) ";
+    char *lineptr;
+    size_t n = 0;
+
+    (void)ac;
+    (void)argv;
+
+    while (1)
+    {
+        printf("%s", prompt);
+        getline(&lineptr, &n, stdin);
+        printf("%s\n", lineptr);
+
+        free(lineptr);
+    }
+    return (0);
+}
