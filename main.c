6a379651c9edd33d694a3cff6524ecf09480bc50
willdeng677-pixel <willdeng677@gmail.com>
2026-08-25 23:51:58 +0000

    Main.c function

--- main.c
+++ main.c
@@ -1,13 +1,46 @@
 #include "shell.h"
 
 /**
- * main - Entry point for the simple shell
+ * trim_command - Removes leading and trailing spaces from a command.
+ * @command: Command to trim.
+ *
+ * Return: Pointer to the trimmed command.
+ */
+char *trim_command(char *command)
+{
+	char *start;
+	char *end;
+
+	start = command;
+
+	while (*start == ' ' || *start == '\t')
+		start++;
+
+	end = start;
+
+	while (*end != '\0')
+		end++;
+
+	end--;
+
+	while (end >= start && (*end == ' ' || *end == '\t'))
+	{
+		*end = '\0';
+		end--;
+	}
+
+	return (start);
+}
+
+/**
+ * main - Entry point for the simple shell.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	char *line = NULL;
+	char *command;
 	char *argv[2];
 	size_t len = 0;
 	ssize_t read;
@@ -25,16 +58,19 @@ int main(void)
 		{
 			if (isatty(STDIN_FILENO))
 				printf("\n");
+
 			break;
 		}
 
 		if (read > 0 && line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
-		if (line[0] == '\0')
+		command = trim_command(line);
+
+		if (command[0] == '\0')
 			continue;
 
-		argv[0] = line;
+		argv[0] = command;
 		argv[1] = NULL;
 
 		pid = fork();
@@ -47,7 +83,7 @@ int main(void)
 
 		if (pid == 0)
 		{
-			if (execve(line, argv, NULL) == -1)
+			if (execve(command, argv, NULL) == -1)
 			{
 				perror("./hsh");
 				exit(127);
@@ -60,5 +96,6 @@ int main(void)
 	}
 
 	free(line);
+
 	return (0);
 }
