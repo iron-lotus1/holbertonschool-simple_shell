dff35561d7e0046c5899d4c1dae569a248c7999e
willdeng677-pixel <willdeng677@gmail.com>
2026-08-25 14:36:58 +0000

    Function which trim space in main function

--- shell.c
+++ shell.c
@@ -1,32 +1,66 @@
 #include "shell.h"
 
 /**
+ * trim_spaces - Removes leading and trailing spaces
+ * @str: String to trim
+ *
+ * Return: Pointer to trimmed string
+ */
+char *trim_spaces(char *str)
+{
+	char *end;
+
+	while (*str == ' ' || *str == '\t')
+		str++;
+
+	if (*str == '\0')
+		return (str);
+
+	end = str + strlen(str) - 1;
+
+	while (end > str && (*end == ' ' || *end == '\t'))
+		end--;
+
+	*(end + 1) = '\0';
+
+	return (str);
+}
+
+/**
  * main - Entry point for the simple shell
  *
- * Return: Always 0
+ * Return: 0 on success
  */
 int main(void)
 {
 	char *line = NULL;
+	char *command;
 	size_t len = 0;
+	ssize_t nread;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("$ ");
 
-		if (getline(&line, &len, stdin) == -1)
+		nread = getline(&line, &len, stdin);
+
+		if (nread == -1)
 		{
-			free(line);
-			return (0);
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			break;
 		}
 
-		line[strcspn(line, "\n")] = '\0';
+		command = trim_spaces(line);
 
-		if (line[0] == '\0')
+		if (*command == '\0')
 			continue;
 
-		execute_command(line);
+		if (handle_builtin(command))
+			continue;
+
+		execute_command(command);
 	}
 
 	free(line);
