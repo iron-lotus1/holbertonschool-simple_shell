1453243f930068f7f992604d15d1e6afae4ad7c7
willdeng677-pixel <willdeng677@gmail.com>
2026-08-25 23:28:54 +0000

    Shell header file

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void print_prompt(void);
+void execute_command(char *command);
+
+#endif
