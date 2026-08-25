3ef4bba60cece41350436c9e50a4f6f6c578dbef
willdeng677-pixel <willdeng677@gmail.com>
2026-08-25 14:31:05 +0000

    shell.h

--- shell.h
+++ shell.h
@@ -3,13 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 extern char **environ;
 
+char *trim_spaces(char *str);
 int execute_command(char *command);
+int handle_builtin(char *command);
 
 #endif /* SHELL_H */
