a88440a4bafe8bd331cf0c175cd4a16ebf13a9dc
willdeng677-pixel <willdeng677@gmail.com>
2026-08-25 23:31:26 +0000

    Main function

---
+++ main.c
@@ -0,0 +1,60 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for the simple shell
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	pid_t pid;
+	int status;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+
+		read = getline(&line, &len, stdin);
+
+		if (read == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			break;
+		}
+
+		if (read > 0 && line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		if (line[0] == '\0')
+			continue;
+
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("./hsh");
+			continue;
+		}
+
+		if (pid == 0)
+		{
+			if (execve(line, (char *[]) {line, NULL}, NULL) == -1)
+			{
+				perror("./hsh");
+				exit(127);
+			}
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+		}
+	}
+
+	free(line);
+	return (0);
+}
