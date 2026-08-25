63ead97154a62e4feb20b18fdffc0a2c9bf8d0a0
willdeng677-pixel <willdeng677@gmail.com>
2026-08-25 13:59:32 +0000

    shell.h

--- shell.h
+++ shell.h
@@ -3,12 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 int execute_command(char *command);
-int handle_builtin(char *command);
 
 #endif /* SHELL_H */
