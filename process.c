dbadc113dfe7e4290add268ce0e972dc9bb26299
Kev <kevin.hulbert93@gmail.com>
2026-08-27 16:42:26 +1000

    update process.c

--- process.c
+++ process.c
@@ -9,11 +9,15 @@
  */
 void process_command(char **args, char **env)
 {
-	if (args == NULL || args[0] == NULL)
-		return;
-
 	if (is_builtin(args))
-		handle_builtin(args, env);
+	{
+		if (strcmp(args[0], "exit") == 0)
+			builtin_exit(args);
+		else if (strcmp(args[0], "env") == 0)
+			builtin_env(env);
+	}
 	else
+	{
 		execute(args, env);
+	}
 }
