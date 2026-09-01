31a7602f34b9490ff2aab9a7e87583d530843149
willdeng677-pixel <willdeng677@gmail.com>
2026-09-01 04:10:41 +0000

    Simple shell header file

--- shell.h
+++ shell.h
@@ -6,13 +6,14 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <string.h>
 
-char *trim_command(char *command);
+int _strlen(char *str);
 int tokenize(char *command, char *argv[]);
+char *find_command(char *command, char **envp);
 char *get_path(char **envp);
 char *build_path(char *directory, char *command);
-char *find_command(char *command, char **envp);
 void print_environment(char **envp);
 
 #endif
